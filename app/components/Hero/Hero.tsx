af14bb23040d325e23031da87d4116d2d193842e
Jesus Orozco <jesusorozco3690@gmail.com>
2024-07-04 22:25:41 -0400

    more

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -6,7 +6,7 @@ import "./Hero.css";
 function Hero() {
   return (
     <div className="hero">
-      <div className="my-auto pt-52">
+      <div className="my-auto pt-14 lg:pt-52">
         <p className="name text-6xl font-bold text-center">jésus orozco</p>
 
         <Image
@@ -19,7 +19,7 @@ function Hero() {
       </div>
 
       <Image
-        className="mx-auto flex flex-col mt-auto mb-0 pb-10"
+        className="mx-auto flex flex-col mt-auto mb-0 pb-20 sm:pb-14 lg:pb-10"
         src="/arrow-down.svg"
         alt=""
         width={48}
--- app/components/Skills/Skills.tsx
+++ app/components/Skills/Skills.tsx
@@ -11,7 +11,23 @@ const Skill: React.FC<SkillProps> = ({ title }) => {
     <div className="skill">
       <p className="text-center">{title}</p>
 
-      <div></div>
+      <div className="relative">
+        <div className="absolute p-6">
+          <p>A</p>
+          <p>B</p>
+          <p>C</p>
+          <p>D</p>
+        </div>
+
+        <Image
+          // md:w-[300px] lg:w-[300px]
+          className="absolute"
+          src="/images/rectangle.png"
+          alt=""
+          width={900}
+          height={900}
+        />
+      </div>
     </div>
   );
 };
@@ -28,12 +44,12 @@ function Skills() {
           className="secondary-underline hidden sm:block sm:mx-auto md:mx-0"
           src="/images/underline.png"
           alt=""
-          width={450}
+          width={500}
           height={300}
         />
       </div>
 
-      <div>
+      <div className="flex items-center justify-center md:justify-evenly flex-col md:flex-row gap-12 mt-16">
         <Skill title="languages" />
 
         <Skill title="frameworks" />
