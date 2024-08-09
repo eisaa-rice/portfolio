adfb92654d3b3648e5d9f5bb74c2baa89b4a37e8
Jesus Orozco <jesusorozco3690@gmail.com>
2024-08-09 01:40:42 -0400

    more

--- app/components/Experience/Experience.tsx
+++ app/components/Experience/Experience.tsx
@@ -13,62 +13,64 @@ interface JobProps {
   title: string;
   desc: string;
 }
-const Job: React.FC<JobProps> = ({ company, link, title, desc }) => {
-  return (
-    <>
-      <div className={"flex flex-col justify-center items-start py-2"}>
-        <div className="flex flex-row items-center justify-center">
-          <p className="text-2xl pb-4">{title}</p>
-        </div>
 
-        <motion.a
-          href={link}
-          target="_blank"
-          className="grid grid-rows-[auto_10px] grid-cols-[auto_auto] pb-2 cursor-pointer"
-          whileHover="hovered"
-          initial="initial"
-        >
-          <p className="text-lg font-bold row-start-1 row-end-2 col-start-1 col-end-2">
-            {company}
-          </p>
+const Job: React.FC<JobProps> = ({ company, link, title, desc }) => (
+  <div className="flex flex-col justify-center items-start pl-12">
+    <div className="flex flex-row items-center justify-center pb-4">
+      <Image
+        className="-ml-12 md:-ml-[6.1rem] rounded-full z-50"
+        src="/images/small-circle.png"
+        alt=""
+        height={32}
+        width={32}
+      />
 
-          <motion.div
-            className="hover-underline row-start-2 row-end-3 col-start-1 col-end-2"
-            variants={{
-              initial: { width: 0 },
-              hovered: { width: "100%" },
-            }}
-            transition={{ ease: "backInOut" }}
-          />
+      <p className="text-2xl pl-4 md:pl-16">{title}</p>
+    </div>
 
-          <motion.div
-            className="row-start-1 row-end-2 col-start-2 col-end-3"
-            variants={{
-              initial: { scale: 1, x: 0, y: 0 },
-              hovered: { scale: 1.25, x: 1, y: 1 },
-            }}
-            transition={{ ease: "backIn" }}
-          >
-            <Image
-              src="/external-link.svg"
-              alt=""
-              height={20}
-              width={20}
-              className="h-full ml-3"
-            />
-          </motion.div>
-        </motion.a>
+    <motion.a
+      href={link}
+      target="_blank"
+      className="grid grid-rows-[auto_10px] grid-cols-[auto_auto] pb-2 cursor-pointer"
+      whileHover="hovered"
+      initial="initial"
+    >
+      <p className="text-lg font-bold row-start-1 row-end-2 col-start-1 col-end-2">
+        {company}
+      </p>
 
-        <p className="text-lg py-4">{desc}</p>
-      </div>
-    </>
-  );
-};
+      <motion.div
+        className="hover-underline row-start-2 row-end-3 col-start-1 col-end-2"
+        variants={{
+          initial: { width: 0 },
+          hovered: { width: "100%" },
+        }}
+        transition={{ ease: "backInOut" }}
+      />
+
+      <motion.div
+        className="row-start-1 row-end-2 col-start-2 col-end-3"
+        variants={{
+          initial: { scale: 1, x: 0, y: 0 },
+          hovered: { scale: 1.25, x: 1, y: 1 },
+        }}
+        transition={{ ease: "backIn" }}
+      >
+        <Image
+          src="/external-link.svg"
+          alt=""
+          height={20}
+          width={20}
+          className="h-full ml-3"
+        />
+      </motion.div>
+    </motion.a>
+
+    <p className="text-lg py-4">{desc}</p>
+  </div>
+);
 
 const Experience: React.FC = () => {
-  const rowStart = [2, 3];
-  const rowEnd = [3, 4];
-
   const workExperience: JobProps[] = [
     {
       time: "CURRENT - MAY 2024",
@@ -101,63 +103,55 @@ const Experience: React.FC = () => {
         here&apos;s where i&apos;ve been
       </motion.p>
 
-      <div className="grid md:grid-cols-[175px_40px_5fr] md:gap-8 items-center h-full mt-16">
-        <Image
-          className="timeline mt-auto md:mx-auto col-start-1 col-end-2 md:col-start-2 md:col-end-3 row-start-1 row-end-2"
-          height={48}
-          width={48}
-          src={"/images/bigger-circle.png"}
-          alt=""
-        />
+      <div className="grid md:grid-cols-[175px_40px_5fr] md:gap-x-8 items-center h-full mt-16">
+        {workExperience.map((job, index) => (
+          <React.Fragment key={index}>
+            <p
+              className={`font-light md:row-start-${index * 2 + 1} md:row-end-${
+                index * 2 + 2
+              } md:col-start-1 md:col-end-2 mt-0 mb-auto md:pt-[0.75rem] pl-12 md:pl-0`}
+            >
+              {job.time}
+            </p>
 
-        <div className="mid-line mx-auto hidden md:block" />
+            <div
+              className={`ml-4 md:ml-auto w-[0.5px] bg-black h-full md:inline-grid md:row-start-${
+                index + 1
+              } md:row-end-${index + 2} md:col-start-2 md:col-end-3`}
+            />
 
-        {workExperience.map((job, index) => {
-          // this is deadass the most ghetto code ive ever written
-
-          return (
-            <React.Fragment key={index}>
-              <Image
-                className={`md:hidden mx-auto mt-2.5 mb-auto md:row-start-${rowStart[index]} md:row-end-${rowEnd[index]} md:col-start-2 md:col-end-3`}
-                src="/images/small-circle.png"
-                alt=""
-                height={30}
-                width={30}
-              />
-
-              <p
-                className={`font-light md:row-start-${rowStart[index]} md:row-end-${rowEnd[index]} md:col-start-1 md:col-end-2 mt-0 mb-auto md:pt-[0.75rem]`}
-              >
-                {job.time}
-              </p>
-
-              <Image
-                className={`hidden md:block mx-auto mt-2.5 mb-auto md:row-start-${rowStart[index]} md:row-end-${rowEnd[index]} md:col-start-2 md:col-end-3`}
-                src="/images/small-circle.png"
-                alt=""
-                height={30}
-                width={30}
-              />
-
-              <div
-                className={`md:row-start-${rowStart[index]} md:row-end-${rowEnd[index]} md:col-start-3 md:col-end-4`}
-              >
-                <Job {...job} />
-              </div>
-            </React.Fragment>
-          );
-        })}
-
-        <Image
-          className="timeline mt-auto md:mx-auto col-start-1 col-end-2 md:col-start-2 md:col-end-3 row-start-8 row-end-9 md:row-start-4 md:row-end-5"
-          height={48}
-          width={48}
-          src={"/images/arrow-down.png"}
-          alt=""
-        />
+            <div
+              className={`md:row-start-${index * 2 + 1} md:row-end-${
+                index * 2 + 2
+              } md:col-start-3 md:col-end-4`}
+            >
+              <Job {...job} />
+            </div>
+          </React.Fragment>
+        ))}
       </div>
     </div>
   );
 };
 
 export default Experience;
+
+{
+  /* <Image
+          className="mt-auto md:mx-auto -ml-2 col-start-1 col-end-2 md:col-start-2 md:col-end-3 row-start-1 row-end-2"
+          height={48}
+          width={48}
+          src={"/images/bigger-circle.png"}
+          alt=""
+        /> */
+}
+
+{
+  /* <Image
+          className="mt-auto md:mx-auto -ml-2 col-start-1 col-end-2 md:col-start-2 md:col-end-3 row-start-8 row-end-9 md:row-start-4 md:row-end-5"
+          height={48}
+          width={48}
+          src={"/images/arrow-down.png"}
+          alt=""
+        /> */
+}
