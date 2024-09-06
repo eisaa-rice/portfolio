7233d11677ebe457132af560ca43a219a2832476
Jesus Orozco <jesusorozco3690@gmail.com>
2024-09-06 01:01:13 -0400

    more

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="text-[#150f03]">
+    <div className="text-black">
       <motion.div
-        className="h-screen w-full bg-[#150f03] absolute top-0 z-30"
+        className="h-screen w-full bg-black absolute top-0 z-30"
         initial={{ height: "100vh" }}
         animate={{ height: 0 }}
         transition={{ duration: 0.5, delay: 1.5, ease: "easeInOut" }}
