13e7cb8d00b28fe593d5848fac0524ab86470c28
Jesus Orozco <jesusorozco3690@gmail.com>
2024-09-21 01:11:32 -0400

    more

--- app/components/About Me/Projects.tsx
+++ app/components/About Me/Projects.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 
 import "./Projects.css";
@@ -8,17 +8,41 @@ import "./Projects.css";
 const Projects = () => {
   const { scrollY } = useScroll();
 
-  const y1 = useTransform(scrollY, [450, 1250], [300, -300]);
-  const rotate1 = useTransform(scrollY, [450, 1250], [5, -5]);
+  const [screenWidth, setScreenWidth] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
 
-  const y2 = useTransform(scrollY, [1400, 2200], [300, -300]);
-  const rotate2 = useTransform(scrollY, [1400, 2200], [5, -5]);
+  useEffect(() => {
+    const updateScreenSize = () => {
+      const width = window.innerWidth;
+
+      setScreenWidth(width);
+      setIsMobile(width <= 768);
+    };
+
+    updateScreenSize();
+    window.addEventListener("resize", updateScreenSize);
+
+    return () => window.removeEventListener("resize", updateScreenSize);
+  }, []);
+
+  const rangeOne = isMobile ? [450, 1450] : [450, 1250];
+  const y1 = useTransform(scrollY, rangeOne, [300, -300]);
+  const rotate1 = useTransform(scrollY, rangeOne, [5, -5]);
+
+  const rangeTwo = isMobile ? [1900, 2900] : [1250, 2250];
+  const y2 = useTransform(scrollY, rangeTwo, [300, -300]);
+  const rotate2 = useTransform(scrollY, rangeTwo, [5, -5]);
+
+  const rangeThree = isMobile ? [3450, 4450] : [2250, 3250];
+  const y3 = useTransform(scrollY, rangeThree, [300, -300]);
+  const rotate3 = useTransform(scrollY, rangeThree, [5, -5]);
 
   return (
-    <div className="min-h-[3000px] w-full flex flex-col items-center justify-start gap-y-[500px]">
+    <div className="w-full flex flex-col items-center justify-start gap-y-[500px]">
       <div className="grid grid-rows-2 grid-cols-1 lg:grid-rows-1 lg:grid-cols-2 lg:gap-x-24">
         <motion.div
           className="row-start-1 row-end-2 col-start-1 col-end-2 lg:col-start-2 lg:col-end-3 bg-white w-64 sm:w-96 h-96 m-auto rounded-2xl"
+          transition={{ delay: 0.5 }}
           style={{ y: y1, rotate: rotate1 }}
         />
 
@@ -84,6 +108,45 @@ const Projects = () => {
             </p>
           </div>
         </div>
+      </div>
+
+      <div className="grid grid-rows-2 grid-cols-1 lg:grid-rows-1 lg:grid-cols-2 lg:gap-x-24">
+        <motion.div
+          className="row-start-1 row-end-2 col-start-1 col-end-2 lg:col-start-2 lg:col-end-3 bg-white w-64 sm:w-96 h-96 m-auto rounded-2xl"
+          style={{ y: y3, rotate: rotate3 }}
+        />
+
+        <div className="row-start-2 row-end-3 lg:row-start-1 lg:row-end-2 col-start-1 col-end-2">
+          <p className="font-semibold text-3xl">project three</p>
+
+          <div className="flex flex-col mt-10 mb-0 gap-y-8">
+            <p className="font-light opacity-75">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sodales dui eget nulla dictum blandit non nec nisl. Cras
+              consectetur mi vitae purus porttitor vestibulum. Sed porta luctus
+              nulla nec rhoncus. Proin massa nibh, rhoncus eu sollicitudin nec,
+              imperdiet in libero.
+            </p>
+
+            <p className="font-light opacity-75">
+              Sed porta luctus nulla nec rhoncus. Proin massa nibh, rhoncus eu
+              sollicitudin nec, imperdiet in libero.
+            </p>
+
+            <p className="font-light opacity-75">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sodales dui eget nulla dictum blandit non nec nisl. Cras
+              consectetur mi vitae purus porttitor vestibulum. Sed porta luctus
+              nulla nec rhoncus. Proin massa nibh, rhoncus eu sollicitudin nec,
+              imperdiet in libero. Nunc rhoncus efficitur est, a suscipit orci
+              laoreet id.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-0 mb-0">
+        <p className="font-light">some other works</p>
       </div>
     </div>
   );
--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -13,7 +13,7 @@ const Hero: React.FC<HeroProps> = () => {
 
   return (
     <div className="hero min-h-screen flex flex-col items-center justify-start mb-72">
-      <div className="flex items-center justify-between w-full mt-5 mb-auto century-gothic">
+      <div className="flex items-center justify-between w-full mt-5 mb-32 century-gothic">
         <div className="opacity-75">
           <p className="text-sm py-1 px-3 bg-white rounded-md bg-opacity-10 ">
             jésus orozco
@@ -56,7 +56,7 @@ const Hero: React.FC<HeroProps> = () => {
       </div>
 
       <div className="flex flex-col items-center justify-center gap-y-12 lg:grid lg:grid-rows-1 lg:grid-cols-2 mb-36 lg:gap-x-12">
-        <div className="hidden md:block sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-2 m-auto">
+        <div className="hidden md:block sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-2 lg:m-auto">
           <motion.div
             className="h-[350px] w-[350px] rounded-full bg-opacity-0 dashed-circle opacity-75 "
             animate={{ rotate: 360 }}
@@ -68,7 +68,7 @@ const Hero: React.FC<HeroProps> = () => {
           />
         </div>
 
-        <div className="lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-2">
+        <div className="lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-2 md:mt-28 lg:mt-0">
           <p className="text-6xl font-medium">aspiring software engineer</p>
 
           <p className="font-light mt-12 opacity-75">
@@ -84,30 +84,28 @@ const Hero: React.FC<HeroProps> = () => {
         </div>
 
         <motion.div
-          className="-mb-32 mx-auto flex flex-col items-center justify-center lg:col-start-1 lg:col-end-3"
-          initial={{ y: 50, opacity: "0%" }}
-          animate={{ y: 15, opacity: "50%" }}
-          transition={{ delay: 4, duration: 1.75, ease: "anticipate" }}
+          className="-mb-32 xl:-mb-52 mx-auto flex flex-col items-center justify-center lg:col-start-1 lg:col-end-3"
+          initial={{ y: 500 }}
+          animate={{ y: 15 }}
+          transition={{ delay: 2.5, duration: 2.5, ease: "anticipate" }}
           style={{ opacity }}
         >
-          <div className="bg-black w-[27px] h-[41px] rounded-3xl absolute z-20" />
-
           <motion.div
-            className="bg-green-300 w-[7px] h-[7px] rounded-full absolute z-30"
             initial={{ y: 0 }}
-            animate={{ y: [10, -10, 10] }}
+            animate={{ y: [5, -5, 5] }}
             transition={{
-              delay: 4,
+              delay: 4.25,
               repeat: Infinity,
               duration: 2,
               ease: "easeInOut",
             }}
-          />
-
-          <div className="bg-slate-50 w-[30px] h-[45px] rounded-3xl absolute -z-20" />
-
-          <motion.div className="mt-16">
-            <Image height={23.5} width={23.5} src={"/down-arrow.svg"} alt="" />
+          >
+            <Image
+              height={120}
+              width={120}
+              src={"/mouse-scroll-icon.svg"}
+              alt=""
+            />
           </motion.div>
         </motion.div>
       </div>
