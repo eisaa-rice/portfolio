b44ef64d7f3788d2dffb165a72a79251de3b77b4
Jesus <jesusorozco3690@gmail.com>
2024-10-14 18:03:28 -0400

    more

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Experience from "./components/Experience/Experience";
-import Skills from "./components/Skills/Skills";
 import Footer from "./components/Footer/Footer";
 
 import { useState, useEffect } from "react";
