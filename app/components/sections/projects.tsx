511d6f8046d0b7ceaa2a91338386dfad02322df8
jésus orozco <jesusorozco3690@gmail.com>
2026-01-06 23:19:16 -0500

    main projects

--- app/components/sections/projects.tsx
+++ app/components/sections/projects.tsx
@@ -6,55 +6,93 @@ import SectionHeader from "../ui/sectionHeader";
 
 const mainProjects = [
   {
-    title: "🍵 brew buzz",
-    desc: "mobile app for local coffee shops integrated with clover's point-of-sales system.",
-    link: "",
-    skills: ["React Native", "Express", "PostgreSQL"],
-  },
-  {
-    title: "👾 hack dearborn",
+    title: "hack dearborn",
+    emoji: "👾",
     desc: "the webpage and mobile app for uofm - dearborn's annual hackathon.",
     link: "https://www.hackdearborn.org/",
     skills: ["React", "React Native", "Supabase"],
   },
   {
-    title: "👨🏿‍💻 bhm summit",
+    title: "bhm summit",
+    emoji: "👨🏿‍💻",
+
     desc: "information hub for a 2025 black history month conference held in detroit.",
     img: "/images/bhm.png",
     link: "https://gdg-summit-webiste.vercel.app/",
     skills: ["React", "JavaScript", "Tailwind CSS"],
   },
   {
-    title: "👩🏼‍💻 iwd summit",
+    title: "iwd summit",
+    emoji: "👩🏼‍💻",
+
     desc: "information hub for a 2024 international women's day conference held in detroit.",
     img: "/images/iwd.png",
     link: "https://www.iwdsummit.com/",
     skills: ["React", "JavaScript", "Tailwind CSS"],
   },
+  {
+    title: "code conductor",
+    emoji: "🤖",
+    desc: "an interactive platform where anyone can design and visualize algorithms.",
+    img: "/images/cc.png",
+    link: "https://www.codeconductor.org/",
+    skills: ["React", "TypeScript", "Material UI"],
+  },
 ];
 
 const MainProject = ({
   title,
+  emoji,
   desc,
   link,
   skills,
 }: {
   title: string;
+  emoji: string;
   desc: string;
   link: string;
   skills: string[];
 }) => {
   return (
     <motion.a
-      className="border border-neutral-300 rounded-lg
-      min-h-48"
+      className="flex flex-col
+      border rounded-lg p-4"
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      initial={{ scale: 1 }}
-      whileHover={{ scale: 1.025, cursor: "pointer" }}
+      whileHover="link"
+      initial={{ scale: 1, borderColor: "#d4d4d4" }}
+      variants={{
+        link: { scale: 1.025, borderColor: "#000000", cursor: "pointer" },
+      }}
     >
-      <p>{title}</p>
+      <div className="flex justify-between mb-2">
+        <p className="text-5xl -ml-1">{emoji}</p>
+
+        <motion.p
+          className="text-xl"
+          initial={{ opacity: 0.25, scale: 1, x: 0, y: 0 }}
+          variants={{ link: { opacity: 1, scale: 1.05, x: 2, y: -2 } }}
+        >
+          ↗
+        </motion.p>
+      </div>
+
+      <h3 className="font-medium text-xl">{title}</h3>
+
+      <p className="mt-2 pb-6">{desc}</p>
+
+      <div className="flex flex-wrap gap-2 mt-auto">
+        {skills.map((skill, i) => (
+          <p
+            key={i}
+            className="text-xs text-neutral-400
+            rounded-full border border-neutral-200 px-2 py-1"
+          >
+            {skill}
+          </p>
+        ))}
+      </div>
     </motion.a>
   );
 };
@@ -146,11 +184,12 @@ const Projects = () => {
     <section id="projects">
       <SectionHeader emoji="💻" title="projects" />
 
-      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 mb-12">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-12">
         {mainProjects.map((mp, i) => (
           <MainProject
             key={i}
             title={mp.title}
+            emoji={mp.emoji}
             desc={mp.desc}
             link={mp.link}
             skills={mp.skills}
